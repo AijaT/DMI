5f570a963b6400e7c053db96e29e3ce4dec3e374
aijatikuma@inbox.lv <aijatikuma@inbox.lv>
2018-01-16 19:58:53 +0200

    20170116

---
+++ PYTHON/mans_cosinuss.py
@@ -0,0 +1,7 @@
+Python 2.7.6 (default, Jun 22 2015, 17:58:13) 
+[GCC 4.8.2] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> import numpy as np
+>>> import matplotlib.pyplot as plt
+
+>>> 
---
+++ mana_cos_ziimeshana.py
@@ -0,0 +1,23 @@
+ # -*- coding: utf-8 -*-
+import matplotlib.pyplot as plt
+import numpy as np
+
+def mans_sinuss(x,n):
+    k = 0
+    a = (-1)**0*x**1/(1)
+    s = a
+    while k < n:
+          k = k + 1
+          R =(-1)*x*x/((2*k)*(2*k+1))
+          a = a *R
+          s = s + a
+
+    return s
+
+x = np.arange (0.0, 6.28, 0.01)
+y = np.sin(x)
+yy = mans_sinuss(x,0)
+
+plt.plot(x,y)
+plt.plot(x,yy)
+plt.show()
